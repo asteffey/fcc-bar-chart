5853a89911d7335bdc4835021287bc5222df6272
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-16 06:48:22 +0000

    Deploy asteffey/fcc-bar-chart to github.com/asteffey/fcc-bar-chart.git:gh-pages

---
+++ precache-manifest.961cbacb96aee9d3bf97f5457d70e7d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9b6703adc0b911e11057cc85d381df",
+    "url": "/fcc-bar-chart/index.html"
+  },
+  {
+    "revision": "3e052d9d90ed6eb588ec",
+    "url": "/fcc-bar-chart/static/css/main.73fe24ea.chunk.css"
+  },
+  {
+    "revision": "89f2491fdbd48f1f6bcf",
+    "url": "/fcc-bar-chart/static/js/2.a676ab54.chunk.js"
+  },
+  {
+    "revision": "3f06f0f9ff8e6a78c5332d3eed7e4fd4",
+    "url": "/fcc-bar-chart/static/js/2.a676ab54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e052d9d90ed6eb588ec",
+    "url": "/fcc-bar-chart/static/js/main.b98c06a5.chunk.js"
+  },
+  {
+    "revision": "043ab25c5c8471f2e9b5",
+    "url": "/fcc-bar-chart/static/js/runtime-main.bf21e64b.js"
+  }
+]);
